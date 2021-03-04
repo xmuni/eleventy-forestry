30c075ce74e5bd953142abd4d9c4525b5c7fdf3d
Vittorio Minuzzo <vitto.m93@gmail.com>
2021-03-04 15:57:08 +0100

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,15 @@
 module.exports = (config) => {
   // config.addPassthroughCopy({'src/_includes/assets/css/main.min.css': 'css/main.min.css'});
-  config.addPassthroughCopy({ "uploads": "uploads" });
-  // config.setFrontMatterParsingOptions({
-  //   excerpt: true,
-  //   excerpt_separator: "<!-- excerpt -->",
-  // });
-  // config.setTemplateFormats(["jpg", "png", "webp", "md", "njk"]);
+  config.addPassthroughCopy({ "src/uploads": "uploads" });
 
-  config.addWatchTarget('_site/assets/*.css');
-  config.setBrowserSyncConfig({
-    files: ['_site/assets/*.css']
-  });
+  config.addWatchTarget("./src/sass/");
 
+  // Base config
   return {
     passthroughFileCopy: true,
-    // dir: {
-    //   input: "./src",
-    //   output: "./build",
-    // },
-  };
+    dir: {
+        input: "src",
+        output: "_site",
+    }
 };
-
-const pluginSass = require("eleventy-plugin-sass");
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginSass, sassPluginOptions);
 };
